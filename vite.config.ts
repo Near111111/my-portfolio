c2506869538445db76ec1434d61281225450c4b5
Near111111 <Jomarmerej@gmail.com>
2025-04-23 18:57:12 +0800

    Fix ports

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: 3000, // Specify the port
+  },
 });
